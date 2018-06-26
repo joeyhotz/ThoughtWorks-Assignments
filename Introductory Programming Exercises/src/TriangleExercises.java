a6b89674c17d5ad6ebccee4d994132f6c148b6a9
Joseph Hotz <joseph.hotz@student.uts.edu.au>
2018-06-26 20:35:55 +1000

    adds menu for exercise selection

--- Introductory Programming Exercises/src/TriangleExercises.java
+++ Introductory Programming Exercises/src/TriangleExercises.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class TriangleExercises {
     public static void main(String[] args) {
-        String output = EasiestExerciseEver.makeOutputString();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hi! Welcome to my ThoughtWorks Assignment.");
+        System.out.println("Type the corresponding number to view the output for the exercise");
+        System.out.println("1: Easiest Exercise Ever");
+        System.out.print("Choose program: ");
+        String input = String.format(scanner.nextLine());
+        String output = "";
+
+        if (input.equals("1")) {
+            output = EasiestExerciseEver.makeOutputString();
+        }
         System.out.print(output);
     }
 }
