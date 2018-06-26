baeb36011db35b4f5c8de4e95789bb948a57911d
Joseph Hotz <joseph.hotz@student.uts.edu.au>
2018-06-26 20:16:25 +1000

    easiest exercise ever complete

---
+++ Introductory Programming Exercises/src/EasiestExerciseEver.java
@@ -0,0 +1,10 @@
+public class EasiestExerciseEver {
+    public static void main(){
+
+    }
+
+    public static String makeOutput(){
+        String output = "*";
+        return output;
+    }
+}
--- Introductory Programming Exercises/src/TriangleExercises.java
+++ Introductory Programming Exercises/src/TriangleExercises.java
@@ -1,5 +1,6 @@
 public class TriangleExercises {
-    public static void main(){
-        
+    public static void main(String[] args) {
+        String output = EasiestExerciseEver.makeOutput();
+        System.out.print(output);
     }
 }
