e0854be03fd96580ccf08e314090006be602757a
Joey Hotz <joeyhotz1@gmail.com>
2018-06-27 16:09:34 +1000

    more accurate naming of main file

--- Introductory Programming Exercises/src/ThoughtWorksExercises.java
+++ Introductory Programming Exercises/src/ThoughtWorksExercises.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TriangleExercises {
+public class ThoughtWorksExercises {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = "";
