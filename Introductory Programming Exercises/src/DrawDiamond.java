81421a0477c909562264d3eb556db0a3bc3ae508
Joey Hotz <joeyhotz1@gmail.com>
2018-06-27 15:32:51 +1000

    draw diamond functionality complete - now draws diamond in grid

--- Introductory Programming Exercises/src/DrawDiamond.java
+++ Introductory Programming Exercises/src/DrawDiamond.java
@@ -1,3 +1,5 @@
+import static java.lang.Math.abs;
+
 public class DrawDiamond {
     public static void main() {}
 
@@ -5,7 +7,13 @@ public class DrawDiamond {
         String output = "";
         for (int y = 1; y <= 2*sideLength-1; y++) { //loops through each line
             for (int x = 1; x <= 2*sideLength-1; x++) { //creates x-axis for required number of characters
-                output = output.concat("*");
+                int leftMargin = abs(y - sideLength);
+                int rightMargin = 2*sideLength - abs(y - sideLength);
+                if (x > leftMargin &&  x < rightMargin) {
+                    output = output.concat("*");
+                } else {
+                    output = output.concat(" ");
+                }
             }
             output = output.concat("\n");
         }
