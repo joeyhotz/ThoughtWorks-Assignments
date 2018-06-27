16fecd4b19a02e9d84019fa2422123bb3fc91887
Joey Hotz <joeyhotz1@gmail.com>
2018-06-27 14:02:17 +1000

    improved menu system (the while loop)

--- Introductory Programming Exercises/src/TriangleExercises.java
+++ Introductory Programming Exercises/src/TriangleExercises.java
@@ -3,21 +3,28 @@ import java.util.Scanner;
 public class TriangleExercises {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Hi! Welcome to my ThoughtWorks Assignment.\nType the corresponding number to view the output for the exercise\n\n");
-        System.out.print("1: Easiest Exercise Ever\n\n");
-        System.out.print("Choose program (or \"x\" for the quitters): ");
-        String input = String.format(scanner.nextLine());
+        String input = "";
         String output = "";
-        while (input != "x") {
+
+        System.out.print("Hi! Welcome to my ThoughtWorks Assignment.\nType the corresponding number to view the output for the exercise\n\n");
+
+        while (!input.equals("x")) {
+            input = "";
+            System.out.print("1: Easiest Exercise Ever\n2: Draw a horizontal line\n\n");
+            System.out.print("Choose program (or \"x\" for the quitters): ");
+            input = String.format(scanner.nextLine());
+
             switch (input) {
                 case "1":   output = EasiestExerciseEver.makeOutputString();
+                            break;
+                case "2":   System.out.print("How many asterisks?: ");
+                            int numberOfAsterisks = Integer.parseInt(scanner.nextLine());
+                            output = DrawHorizontalLine.makeOutputString(numberOfAsterisks);
                             break;
                 case "x":   output = "That's all folks!";
                             break;
             }
-            System.out.print(output + "\n\n");
-            System.out.print("Choose program (or \"x\" for the quitters): ");
-            input = String.format(scanner.nextLine());
+            System.out.print(output + "\n\n\n");
         }
     }
 }
