4bce4a5caded8cd30ca847138c84ab4456e831b2
Joey Hotz <joeyhotz1@gmail.com>
2018-06-27 13:46:24 +1000

    adds improved menu - wording and quit feature

--- Introductory Programming Exercises/src/TriangleExercises.java
+++ Introductory Programming Exercises/src/TriangleExercises.java
@@ -3,16 +3,21 @@ import java.util.Scanner;
 public class TriangleExercises {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hi! Welcome to my ThoughtWorks Assignment.");
-        System.out.println("Type the corresponding number to view the output for the exercise");
-        System.out.println("1: Easiest Exercise Ever");
-        System.out.print("Choose program: ");
+        System.out.print("Hi! Welcome to my ThoughtWorks Assignment.\nType the corresponding number to view the output for the exercise\n\n");
+        System.out.print("1: Easiest Exercise Ever\n\n");
+        System.out.print("Choose program (or \"x\" for the quitters): ");
         String input = String.format(scanner.nextLine());
         String output = "";
-
-        if (input.equals("1")) {
-            output = EasiestExerciseEver.makeOutputString();
+        while (input != "x") {
+            switch (input) {
+                case "1":   output = EasiestExerciseEver.makeOutputString();
+                            break;
+                case "x":   output = "That's all folks!";
+                            break;
+            }
+            System.out.print(output + "\n\n");
+            System.out.print("Choose program (or \"x\" for the quitters): ");
+            input = String.format(scanner.nextLine());
         }
-        System.out.print(output);
     }
 }
