4d8df1a7568808681f081ebcd763e0a87ca867b1
Joey Hotz <joeyhotz1@gmail.com>
2018-06-27 14:56:30 +1000

    completes draw isosceles triangle functionality

--- Introductory Programming Exercises/src/DrawIsoscelesTriangle.java
+++ Introductory Programming Exercises/src/DrawIsoscelesTriangle.java
@@ -5,7 +5,13 @@ public class DrawIsoscelesTriangle {
         String output = "";
         for (int y = 1; y <= numberOfLines; y++) { //loops through each line
             for (int x = 1; x <= 2*numberOfLines - 1; x++) { //creates x-axis for required number of characters
-                output = output.concat("x");
+                int leftMargin = numberOfLines - y;
+                int rightMargin = numberOfLines + y;
+                if (x > leftMargin &&  x < rightMargin) {
+                    output = output.concat("*");
+                } else {
+                    output = output.concat(" ");
+                }
             }
             output = output.concat("\n");
         }
