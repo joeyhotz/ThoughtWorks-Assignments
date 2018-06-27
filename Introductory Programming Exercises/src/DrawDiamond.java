d7ab10808761ee62d06e692f8eaca2ed6b5d61a3
Joseph Hotz <joseph.hotz@student.uts.edu.au>
2018-06-27 17:45:36 +1000

    adds a few more comments

--- Introductory Programming Exercises/src/DrawDiamond.java
+++ Introductory Programming Exercises/src/DrawDiamond.java
@@ -5,8 +5,8 @@ public class DrawDiamond {
 
     public static String makeOutputString(int sideLength) {
         String output = "";
-        for (int y = 1; y <= 2*sideLength-1; y++) { //loops through each line
-            for (int x = 1; x <= 2*sideLength-1; x++) { //creates x-axis for required number of characters
+        for (int y = 1; y <= 2*sideLength-1; y++) { //loops through each line (the y axis of the grid)
+            for (int x = 1; x <= 2*sideLength-1; x++) { //loops through each x spot (the x axis of the grid)
                 int leftMargin = abs(y - sideLength);
                 int rightMargin = 2*sideLength - abs(y - sideLength);
                 if (x > leftMargin &&  x < rightMargin) {
